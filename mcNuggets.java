8c240fc907fd23181a89052abb860f0ddc287e12
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-14 19:46:41 +0000

    Add code that loops every value of S * 6 + N * 9 + T * 20

--- mcNuggets.java
+++ mcNuggets.java
@@ -9,5 +9,6 @@ public class mcNuggets {
 	for (int sum = 1; sum <= Nuggets; sum++) { //Highest amount of nuggets less than or equal to the user input
 		for (int S = 0; S * 6 <= sum; S++) { //Highest amount of 6 pack nuggets until the amount is greater than or equal to user input"
 			for (int N = 0; S * 6 + N * 9 <= sum; N++) { //Loops for every value of S*6 + N*9, S is always constant
-				
+				for ( int T = 0; S * 6 + N * 9 + T * 20 <= sum; T++) { //Loops for every value of S*6 + N*9 + T*20, S is always constant
+					
 
