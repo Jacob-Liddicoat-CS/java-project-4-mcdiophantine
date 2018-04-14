b12f481dea95e68aa6fd9d351fff886bee8a22c4
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-14 19:21:51 +0000

    Add scanner to scan the amount mcnuggets the program will run up to

--- mcNuggets.java
+++ mcNuggets.java
@@ -3,3 +3,6 @@ import java.util.*;
 
 public class mcNuggets {
 	public static void main(String args[]) {
+	Scanner kbReader = new Scanner(System.in);//Scanner to scan the number you input
+	System.out.print("Enter the max amount of Mcnuggets the program will run to: ");
+	
