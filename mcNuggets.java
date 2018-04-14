e76688ad9c082b73133ace2e3a2ae529a43bf2c7
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-14 19:29:56 +0000

    Add code to calculate the highest amount of nuggets based on user input

--- mcNuggets.java
+++ mcNuggets.java
@@ -5,4 +5,7 @@ public class mcNuggets {
 	public static void main(String args[]) {
 	Scanner kbReader = new Scanner(System.in);//Scanner to scan the number you input
 	System.out.print("Enter the max amount of Mcnuggets the program will run to: ");
+	int Nuggets = kbReader.nextInt();
+	for (int sum = 1; sum <= Nuggets; sum++) { //Highest amount of nuggets less than or equal to the user input
 	
+
